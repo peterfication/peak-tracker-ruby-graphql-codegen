a6fb571f1641ea7ad0c1dbae80b197bacbc481fa
Peter Morgenstern <mail@petergundel.de>
2023-11-02 08:21:58 +0100

    Add GraphQL types output to codegen

--- graphql-codegen-peak-tracker.js
+++ graphql-codegen-peak-tracker.js
@@ -16,9 +16,291 @@ module.exports = {
       'end',
     ];
 
+    const enumsOutput = customEnumTypeKeys(typesMap).map((typeKey) =>
+      outputEnumType(typesMap[typeKey]),
+    );
+
+    const typesOutput = customObjectTypeKeys(typesMap).map((typeKey) =>
+      outputObjectType(typesMap[typeKey], getCustomTypeNames(typesMap)),
+    );
+
+    const argumentsOutput = outputArgumentsModule(typesMap);
+
     return [
       ...generalOutput,
+      ...enumsOutput,
+      ...typesOutput,
+      ...argumentsOutput,
       '',
     ].join('\n');
   },
+};
+
+// These types are ignored as they are built into the GraphQL Ruby schema
+const ignoredTypes = [
+  'ID',
+  'Int',
+  'Float',
+  'ISO8601DateTime',
+  'ISO8601Date',
+  'Boolean',
+  'String',
+  'Query',
+  'RootQueryType',
+  'Node',
+];
+
+/**
+ * Get all custom enum type keys to generate enum type classes with it.
+ */
+const customEnumTypeKeys = (typesMap) =>
+  customTypeKeys(typesMap).filter(
+    (typeKey) => typesMap[typeKey].constructor.name === 'GraphQLEnumType',
+  );
+
+/**
+ * Get all custom object type keys to generate object type classes with it.
+ */
+const customObjectTypeKeys = (typesMap) =>
+  customTypeKeys(typesMap).filter(
+    (typeKey) => typesMap[typeKey].constructor.name !== 'GraphQLEnumType',
+  );
+
+/**
+ * Get all custom type keys, meaning types that are special to the peak_tracker
+ * service and not the general ones created by GraphQL Ruby.
+ */
+const customTypeKeys = (typesMap) =>
+  Object.keys(typesMap)
+    .filter(
+      (typeKey) =>
+        !ignoredTypes.includes(typesMap[typeKey].name) &&
+        !typesMap[typeKey].name.startsWith('__'),
+    )
+    .sort((a, b) => getOrder(a) - getOrder(b));
+
+/**
+ * Get all the custom type names as an array so they can be used
+ * in getTypeClass.
+ */
+const getCustomTypeNames = (typesMap) =>
+  customTypeKeys(typesMap).map((customTypeKey) => typesMap[customTypeKey].name);
+
+const camelToSnakeCase = (str) =>
+  str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+
+const pascalToConstantCase = (str) =>
+  camelToSnakeCase(str).toUpperCase().slice(1);
+
+const order = ['PageInfo'];
+/**
+ * Order the type definitions so that a single Ruby file
+ * won't have problems with classes that are not defined yet.
+ */
+const getOrder = (typeKey) =>
+  order.includes(typeKey) ? order.indexOf(typeKey) : 999;
+
+/**
+ * Get the Ruby type class for the type of a field.
+ */
+const getTypeClass = (type, customTypeNames) => {
+  const typeClass = type.ofType?.name || type.name;
+
+  if (type.ofType?.constructor?.name === 'GraphQLList') {
+    return `[${getTypeClass(type.ofType.ofType, customTypeNames)}]`;
+  }
+
+  if (type.constructor?.name === 'GraphQLList') {
+    return `[${getTypeClass(type.ofType, customTypeNames)}]`;
+  }
+
+  if (type.ofType?.constructor?.name === 'GraphQLEnumType') {
+    return `PeakTracker::Types::Enums::${typeClass}`;
+  }
+
+  if (customTypeNames.includes(typeClass)) {
+    return `PeakTracker::Types::${typeClass}`;
+  }
+
+  return `GraphQL::Types::${typeClass}`;
+};
+
+/**
+ * Generate the Ruby output for an object type.
+ */
+const outputObjectType = (objectType, customTypes) => {
+  const outputHeader = [
+    '',
+    'module PeakTracker',
+    '  module Types',
+    `    class ${objectType.name} < Customer::Types::BaseObject`,
+    `      graphql_name "PeakTracker${objectType.name}"`,
+    `      description "${(objectType.description || '').replace(/\n$/g, '')}"`,
+    '',
+  ];
+
+  const fields =
+    typeof objectType.getFields === 'function' ? objectType.getFields() : {};
+
+  const outputFields = Object.keys(fields).map((fieldKey) => {
+    const field = fields[fieldKey];
+
+    const nullValue = field.type.constructor.name !== 'GraphQLNonNull';
+    const typeClass = getTypeClass(field.type, customTypes);
+    const description = (field.description || '')
+      .replace(/\n$/g, '')
+      .replaceAll('"', '\\"');
+
+    return [
+      `      field :${camelToSnakeCase(field.name)}`,
+      typeClass,
+      `null: ${nullValue}`,
+      `description: "${description}"`,
+    ].join(', ');
+  });
+
+  const outputFooter = ['    end', '  end', 'end'];
+
+  return [...outputHeader, ...outputFields, ...outputFooter].join('\n');
+};
+
+/**
+ * Ignore the arguments that come from GraphQL Ruby
+ */
+const ignoredArguments = ['id', 'first', 'after', 'last', 'before'];
+
+const outputArgumentsModule = (typesMap) => {
+  const outputHeader = [
+    '',
+    '# rubocop:disable Style/TrailingCommaInHashLiteral',
+    'module PeakTracker',
+    '  module Types',
+    '    module Arguments',
+    '      ##',
+    '      # Example usage:',
+    '      # field(...) do',
+    '      #   PeakTracker::Types::Arguments.define_arguments(self, :PEAK, :scaled_by_user, %i[user_id])',
+    '      # end',
+    '      def self.define_arguments(type, constant, field, keys)',
+    '        keys.each do |arg|',
+    '          definition = "PeakTracker::Types::Arguments::#{constant}".constantize[field][arg]',
+    '          type.argument(',
+    '            arg,',
+    '            definition[:type],',
+    '            required: definition[:required],',
+    '            description: definition[:description]',
+    '          )',
+    '        end',
+    '      end',
+  ];
+
+  const argumentsOutput = customObjectTypeKeys(typesMap)
+    .map((typeKey) =>
+      outputArguments(typesMap[typeKey], getCustomTypeNames(typesMap)),
+    )
+    .filter((output) => output !== '')
+    .flatMap((output) => ['', ...output]);
+
+  const outputFooter = [
+    '    end',
+    '  end',
+    'end',
+    '# rubocop:enable Style/TrailingCommaInHashLiteral',
+  ];
+
+  return [...outputHeader, ...argumentsOutput, ...outputFooter];
+};
+/**
+ * Generate the Ruby output for an object type.
+ */
+const outputArguments = (objectType, customTypes) => {
+  const outputHeader = [
+    `      ${pascalToConstantCase(objectType.name)} = {`,
+  ];
+
+  const fields =
+    typeof objectType.getFields === 'function' ? objectType.getFields() : {};
+
+  const argumentsOutput = Object.keys(fields)
+    .map((fieldKey) => {
+      const field = fields[fieldKey];
+
+      const fieldOutputHeader = [`        ${camelToSnakeCase(field.name)}: {`];
+
+      const fieldOutputArguments = (field.args || [])
+        // Ignore the arguments that come from GraphQL Ruby
+        .filter((arg) => !ignoredArguments.includes(arg.name))
+        .map((arg) => {
+          const typeClass = getTypeClass(arg.type, customTypes);
+          const description = (arg.description || '').replace(/\n$/g, '');
+          const required = arg.type.constructor.name === 'GraphQLNonNull';
+
+          return [
+            `          ${camelToSnakeCase(arg.name)}: {`,
+            `            type: ${typeClass},`,
+            `            required: ${required},`,
+            `            description: "${description}",`,
+            // The default value should not be shown at all if there is no default value
+            arg.defaultValue
+              ? `            default_value: ${arg.defaultValue},`
+              : undefined,
+            `          },`,
+          ]
+            .filter((output) => output !== undefined)
+            .join('\n');
+        });
+
+      const fieldOutputFooter = [`        },`];
+
+      // If there are no relevant arguments, we don't want to generated an empty Hash
+      if (fieldOutputArguments.length === 0) {
+        return '';
+      }
+
+      return [
+        ...fieldOutputHeader,
+        ...fieldOutputArguments,
+        ...fieldOutputFooter,
+      ].join('\n');
+    })
+    .filter((output) => output !== '');
+
+  // If there are no relevant arguments, we don't want to generate anything
+  if (argumentsOutput.length === 0) {
+    return '';
+  }
+
+  const outputFooter = [
+    '      }.freeze',
+  ];
+
+  return [...outputHeader, ...argumentsOutput, ...outputFooter];
+};
+
+/**
+ * Generate the Ruby output for an enum type.
+ */
+const outputEnumType = (enumType) => {
+  console.log(enumType);
+  const outputHeader = [
+    '',
+    'module PeakTracker',
+    '  module Types',
+    '    module Enums',
+    `      class ${enumType.name} < Customer::Types::BaseEnum`,
+    `        graphql_name "PeakTracker${enumType.name}"`,
+    `        description "${(enumType.description || '').replace(/\n$/g, '')}"`,
+    '',
+  ];
+
+  const values = enumType.getValues();
+  const outputValues = Object.keys(values).map((valueKey) => {
+    const value = values[valueKey];
+
+    return `        value "${value.name}", "${value.description}"`;
+  });
+
+  const outputFooter = ['      end', '    end', '  end', 'end'];
+
+  return [...outputHeader, ...outputValues, ...outputFooter].join('\n');
 };
--- lib/peak_tracker/graphql_peak_tracker.rb
+++ lib/peak_tracker/graphql_peak_tracker.rb
@@ -5,3 +5,419 @@
 # This is just for Ruby auto loading to be happy
 module GraphqlPeakTracker
 end
+
+module PeakTracker
+  module Types
+    module Enums
+      class PeakSortField < Customer::Types::BaseEnum
+        graphql_name "PeakTrackerPeakSortField"
+        description ""
+
+        value "ELEVATION", "null"
+        value "GOOGLE_PLACES_ID", "null"
+        value "ID", "null"
+        value "LATITUDE", "null"
+        value "LONGITUDE", "null"
+        value "NAME", "null"
+        value "OSM_ID", "null"
+        value "SCALED_BY_USER", "null"
+        value "SCALE_COUNT", "null"
+        value "SLUG", "null"
+        value "WIKIDATA_ID", "null"
+        value "WIKIPEDIA", "null"
+      end
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    module Enums
+      class SortOrder < Customer::Types::BaseEnum
+        graphql_name "PeakTrackerSortOrder"
+        description ""
+
+        value "ASC", "null"
+        value "ASC_NULLS_FIRST", "null"
+        value "ASC_NULLS_LAST", "null"
+        value "DESC", "null"
+        value "DESC_NULLS_FIRST", "null"
+        value "DESC_NULLS_LAST", "null"
+      end
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PageInfo < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPageInfo"
+      description "A relay page info"
+
+      field :end_cursor, GraphQL::Types::String, null: true, description: "When paginating forwards, the cursor to continue"
+      field :has_next_page, GraphQL::Types::Boolean, null: false, description: "When paginating forwards, are there more items?"
+      field :has_previous_page, GraphQL::Types::Boolean, null: false, description: "When paginating backwards, are there more items?"
+      field :start_cursor, GraphQL::Types::String, null: true, description: "When paginating backwards, the cursor to continue"
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class Peak < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeak"
+      description ""
+
+      field :elevation, GraphQL::Types::Int, null: false, description: ""
+      field :google_places_id, GraphQL::Types::String, null: true, description: ""
+      field :id, GraphQL::Types::ID, null: false, description: ""
+      field :latitude, GraphQL::Types::Float, null: false, description: ""
+      field :longitude, GraphQL::Types::Float, null: false, description: ""
+      field :name, GraphQL::Types::String, null: false, description: ""
+      field :osm_id, GraphQL::Types::Int, null: false, description: ""
+      field :scale_count, GraphQL::Types::Int, null: false, description: ""
+      field :scaled_by_user, GraphQL::Types::Boolean, null: true, description: ""
+      field :slug, GraphQL::Types::String, null: false, description: ""
+      field :wikidata_id, GraphQL::Types::String, null: true, description: ""
+      field :wikipedia, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakConnection < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakConnection"
+      description ":peak connection"
+
+      field :count, GraphQL::Types::Int, null: true, description: "Total count on all pages"
+      field :edges, [PeakTracker::Types::PeakEdge], null: true, description: ":peak edges"
+      field :page_info, PeakTracker::Types::PageInfo, null: false, description: "Page information"
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakEdge < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakEdge"
+      description ":peak edge"
+
+      field :cursor, GraphQL::Types::String, null: false, description: "Cursor"
+      field :node, PeakTracker::Types::Peak, null: true, description: ":peak node"
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterElevation < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterElevation"
+      description ""
+
+      field :eq, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :in, [GraphQL::Types::Int], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Int, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :not_eq, GraphQL::Types::Int, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterGooglePlacesId < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterGooglePlacesId"
+      description ""
+
+      field :eq, GraphQL::Types::String, null: true, description: ""
+      field :greater_than, GraphQL::Types::String, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :in, [GraphQL::Types::String], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::String, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :not_eq, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterId < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterId"
+      description ""
+
+      field :eq, GraphQL::Types::ID, null: true, description: ""
+      field :greater_than, GraphQL::Types::ID, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::ID, null: true, description: ""
+      field :in, [GraphQL::Types::ID], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::ID, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::ID, null: true, description: ""
+      field :not_eq, GraphQL::Types::ID, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterInput < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterInput"
+      description ""
+
+      field :and, [PeakTracker::Types::PeakFilterInput], null: true, description: ""
+      field :elevation, PeakTracker::Types::PeakFilterElevation, null: true, description: ""
+      field :google_places_id, PeakTracker::Types::PeakFilterGooglePlacesId, null: true, description: ""
+      field :id, PeakTracker::Types::PeakFilterId, null: true, description: ""
+      field :latitude, PeakTracker::Types::PeakFilterLatitude, null: true, description: ""
+      field :longitude, PeakTracker::Types::PeakFilterLongitude, null: true, description: ""
+      field :name, PeakTracker::Types::PeakFilterName, null: true, description: ""
+      field :not, [PeakTracker::Types::PeakFilterInput], null: true, description: ""
+      field :or, [PeakTracker::Types::PeakFilterInput], null: true, description: ""
+      field :osm_id, PeakTracker::Types::PeakFilterOsmId, null: true, description: ""
+      field :scale_count, PeakTracker::Types::PeakFilterScaleCount, null: true, description: ""
+      field :scaled_by_user, PeakTracker::Types::PeakFilterScaledByUser, null: true, description: ""
+      field :slug, PeakTracker::Types::PeakFilterSlug, null: true, description: ""
+      field :wikidata_id, PeakTracker::Types::PeakFilterWikidataId, null: true, description: ""
+      field :wikipedia, PeakTracker::Types::PeakFilterWikipedia, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterLatitude < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterLatitude"
+      description ""
+
+      field :eq, GraphQL::Types::Float, null: true, description: ""
+      field :greater_than, GraphQL::Types::Float, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Float, null: true, description: ""
+      field :in, [GraphQL::Types::Float], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Float, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Float, null: true, description: ""
+      field :not_eq, GraphQL::Types::Float, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterLongitude < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterLongitude"
+      description ""
+
+      field :eq, GraphQL::Types::Float, null: true, description: ""
+      field :greater_than, GraphQL::Types::Float, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Float, null: true, description: ""
+      field :in, [GraphQL::Types::Float], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Float, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Float, null: true, description: ""
+      field :not_eq, GraphQL::Types::Float, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterName < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterName"
+      description ""
+
+      field :eq, GraphQL::Types::String, null: true, description: ""
+      field :greater_than, GraphQL::Types::String, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :in, [GraphQL::Types::String], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::String, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :not_eq, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterOsmId < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterOsmId"
+      description ""
+
+      field :eq, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :in, [GraphQL::Types::Int], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Int, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :not_eq, GraphQL::Types::Int, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterScaleCount < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterScaleCount"
+      description ""
+
+      field :eq, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than, GraphQL::Types::Int, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :in, [GraphQL::Types::Int], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Int, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Int, null: true, description: ""
+      field :not_eq, GraphQL::Types::Int, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterScaledByUser < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterScaledByUser"
+      description ""
+
+      field :eq, GraphQL::Types::Boolean, null: true, description: ""
+      field :greater_than, GraphQL::Types::Boolean, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::Boolean, null: true, description: ""
+      field :in, [GraphQL::Types::Boolean], null: true, description: ""
+      field :input, PeakTracker::Types::PeakScaledByUserFieldInput, null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::Boolean, null: true, description: ""
+      field :not_eq, GraphQL::Types::Boolean, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterSlug < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterSlug"
+      description ""
+
+      field :eq, GraphQL::Types::String, null: true, description: ""
+      field :greater_than, GraphQL::Types::String, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :in, [GraphQL::Types::String], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::String, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :not_eq, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterWikidataId < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterWikidataId"
+      description ""
+
+      field :eq, GraphQL::Types::String, null: true, description: ""
+      field :greater_than, GraphQL::Types::String, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :in, [GraphQL::Types::String], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::String, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :not_eq, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakFilterWikipedia < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakFilterWikipedia"
+      description ""
+
+      field :eq, GraphQL::Types::String, null: true, description: ""
+      field :greater_than, GraphQL::Types::String, null: true, description: ""
+      field :greater_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :in, [GraphQL::Types::String], null: true, description: ""
+      field :is_nil, GraphQL::Types::Boolean, null: true, description: ""
+      field :less_than, GraphQL::Types::String, null: true, description: ""
+      field :less_than_or_equal, GraphQL::Types::String, null: true, description: ""
+      field :not_eq, GraphQL::Types::String, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakScaledByUserFieldInput < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakScaledByUserFieldInput"
+      description ""
+
+      field :user_id, GraphQL::Types::ID, null: false, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class PeakSortInput < Customer::Types::BaseObject
+      graphql_name "PeakTrackerPeakSortInput"
+      description ""
+
+      field :field, PeakTracker::Types::Enums::PeakSortField, null: false, description: ""
+      field :order, PeakTracker::Types::SortOrder, null: true, description: ""
+      field :scaled_by_user_input, PeakTracker::Types::PeakScaledByUserFieldInput, null: true, description: ""
+    end
+  end
+end
+
+module PeakTracker
+  module Types
+    class User < Customer::Types::BaseObject
+      graphql_name "PeakTrackerUser"
+      description ""
+
+      field :email, GraphQL::Types::String, null: false, description: ""
+      field :id, GraphQL::Types::ID, null: false, description: ""
+    end
+  end
+end
+
+# rubocop:disable Style/TrailingCommaInHashLiteral
+module PeakTracker
+  module Types
+    module Arguments
+      ##
+      # Example usage:
+      # field(...) do
+      #   PeakTracker::Types::Arguments.define_arguments(self, :PEAK, :scaled_by_user, %i[user_id])
+      # end
+      def self.define_arguments(type, constant, field, keys)
+        keys.each do |arg|
+          definition = "PeakTracker::Types::Arguments::#{constant}".constantize[field][arg]
+          type.argument(
+            arg,
+            definition[:type],
+            required: definition[:required],
+            description: definition[:description]
+          )
+        end
+      end
+
+      PEAK = {
+        scaled_by_user: {
+          user_id: {
+            type: GraphQL::Types::ID,
+            required: true,
+            description: "",
+          },
+        },
+      }.freeze
+    end
+  end
+end
+# rubocop:enable Style/TrailingCommaInHashLiteral
